81552d941b9cb62cc2418ffbc640b99deb32abe2
Micah B <46792306+Tkod1@users.noreply.github.com>
2025-03-20 12:22:31 -0700

    Update main.py
    Going to create db and extend to aws

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
-#allow frontend to fetch data from fastAPI
+
+
+#allow frontend to fetch data from the  backend
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # need to change this to my domian once in prod
     allow_credintals=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,9 +20,17 @@ app.add_middleware(
 pickleball_courts = [
     {"lat": 37.7749, "lng": -122.4194},  # San Francisco
     {"lat": 34.0522, "lng": -118.2437},  # Los Angeles
-    {"lat": 40.7128, "lng": -74.0060},   # New York
 ]
+# define request model
+class Court(BaseModel):
+    lat: float
+    lng: float
 
 @app.get("/api/courts")
 def get_courts():
     return pickleball_courts
+
+@app.post("/api/add_court")
+def add_court(court:Court):
+    pickleball_courts.append({"lat":court.lat, "lng":court.lng})
+    return{"message": "Court was successfully added!", "lat":court.lat, "lng": court.lng}
